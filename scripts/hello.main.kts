dc1555d1e856343d81f9aac9321db03c60563fa8
Ted Naleid <contact@naleid.com>
2020-07-09 00:34:42 -0500

    add hello world simple example

---
+++ scripts/hello.main.kts
@@ -0,0 +1,35 @@
+#!/bin/sh
+//usr/bin/env true; exec kotlinc -script "$0" -- "$@"  # better than #!/usr/bin/env kotlin as that doesn't support switches without "--" prefix
+
+@file:DependsOn("com.github.ajalt:clikt:2.7.1")
+
+import com.github.ajalt.clikt.core.CliktCommand
+import com.github.ajalt.clikt.parameters.options.default
+import com.github.ajalt.clikt.parameters.options.option
+import com.github.ajalt.clikt.parameters.types.defaultStdin
+import com.github.ajalt.clikt.parameters.types.inputStream
+import com.github.ajalt.clikt.parameters.types.int
+
+class Hello : CliktCommand(
+    help = """try: echo "Ted\nWorld" | scripts/hello.main.kts --count 3
+
+Help lines that keep their newlines:
+```
+one
+two
+``` """
+) {
+
+    private val count: Int by option(help = "a number of times to say hello to each line, default 2").int().default(2)
+    private val input by option(help = "input, defaults to stdin").inputStream().defaultStdin()
+
+    override fun run() {
+        input.bufferedReader().lines().forEach { line ->
+            (1..count).forEach { _ ->
+                echo("Hello $line")
+            }
+        }
+    }
+}
+
+Hello().main(args)
