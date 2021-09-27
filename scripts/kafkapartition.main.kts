a550de14dba8ab9d67919b37a115fdfe2bbfd5e7
Ted Naleid <contact@naleid.com>
2021-09-27 17:19:27 -0500

    fix default kafka partitioner algorithm, positive not absolute

--- scripts/kafkapartition.main.kts
+++ scripts/kafkapartition.main.kts
@@ -10,32 +10,29 @@ import com.github.ajalt.clikt.parameters.types.defaultStdin
 import com.github.ajalt.clikt.parameters.types.inputStream
 import com.github.ajalt.clikt.parameters.types.int
 import org.apache.commons.codec.digest.MurmurHash2
-import kotlin.math.absoluteValue
-
 
 class KeyPartition : CliktCommand(
-    help = """Calculates a murmur2_32 hash for each line it is piped via stdin.
+    help = """
+Calculates the kafka partition using the Kafka default partitioning strategy for each line it is piped via stdin.  
         
-The default Kafka partitioner uses this for keyed events.
-
 It will emit the original value followed by the partition value, ex:
 ```
 seq 1000 1004 | scripts/kafkapartition.main.kts --partitions=10
 1000 6
 1001 8
-1002 7
+1002 1
 1003 3
-1004 6
+1004 2
 ```
 
 It can also be passed a regular expression to calculate a hash on part of the line, and will emit the line,
 the hashed portion, and the partition, ex:
 ```
 seq 1000 1004 | scripts/kafkapartition.main.kts --partitions=10 --regex '\d{3}${'$'}'
-1000 000 6
-1001 001 6
+1000 000 2
+1001 001 2
 1002 002 8
-1003 003 8
+1003 003 0
 1004 004 3
 ``` """
 ) {
@@ -44,7 +41,12 @@ seq 1000 1004 | scripts/kafkapartition.main.kts --partitions=10 --regex '\d{3}${
     private val regex: String? by option(help = "Regular expression to extract only a portion the line to hash, ex: \"^.*:.*[^:]\"")
     private val input by option(help = "Lines to hash, defaults to using stdin").inputStream().defaultStdin()
 
-    private fun bucketFor(value: String, buckets: Int) = MurmurHash2.hash32(value).absoluteValue % buckets
+    // used by the kafka default partitioning strategy, does NOT use absoluteValue to get a positive value
+    // see: https://github.com/apache/kafka/blob/2.8/clients/src/main/java/org/apache/kafka/clients/producer/internals/DefaultPartitioner.java#L71
+    // and: https://github.com/apache/kafka/blob/2.8/clients/src/main/java/org/apache/kafka/common/utils/Utils.java#L1019-L1034
+    private fun Int.positiveValue(): Int = this and 0x7fffffff
+
+    private fun bucketFor(value: String, buckets: Int) = MurmurHash2.hash32(value).positiveValue() % buckets
 
     private fun hashByLine() {
         input.bufferedReader().lines().forEach { line ->
